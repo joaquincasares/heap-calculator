4875e76c50ca655f6b5726457c8a2d3f35e5ff2b
Joaquin Casares <joaquin@datastax.com>
2012-03-01 19:44:19 -0600

    Code comments

--- calculator.py
+++ calculator.py
@@ -2,6 +2,7 @@
 
 import sys
 
+# Ensure command line arguments are given
 if len(sys.argv) > 2:
     filename = sys.argv[1]
     heapsize = int(sys.argv[2]) * 1000
@@ -9,8 +10,12 @@ else:
     sys.stderr.write('Usage: <filename> <heapsize-in-GB>\n')
     sys.exit(0)
 
+
+# Default memtable size is 1/3 of heapspace
 memtable_total_space_in_mb = heapsize / 3
 
+
+# Calculate Key Cache
 with open(filename, 'r') as f:
     cfstats = f.read()
 
@@ -25,12 +30,18 @@ for line in cfstats.split('\n'):
     if needle in line:
         row_cache_estimate += int(line.split(needle)[1])
 
+# For the difference in reported Java heap usage and actual size
 key_cache_estimate *= 12
 row_cache_estimate *= 12
 
+
+# Print out the Calculated Heap Sizes
 print 'Estimated Java Heap Size:'
 print memtable_total_space_in_mb + 1000 + 0 + key_cache_estimate
 
+# Print another estimation if row cache is present
+# May not be necessary since this off heap by default in 1.0+
 if row_cache_estimate:
+    print
     print 'Estimated Java Heap Size (w/Row Cache Sizes):'
     print memtable_total_space_in_mb + 1000 + 0 + key_cache_estimate + row_cache_estimate
